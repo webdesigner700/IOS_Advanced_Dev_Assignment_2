36386d6dfdb2e974ced8b6fc68892f05289ce94c
Vinay B <vinay.bayyapunedi@gmail.com>
2023-09-05 11:10:54 +1000

    Initial Commit

---
+++ Assignment 2/Assignment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_2App.swift
+//  Assignment 2
+//
+//  Created by vinay bayyapunedi on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
